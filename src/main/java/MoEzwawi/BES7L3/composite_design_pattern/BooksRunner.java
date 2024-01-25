7e99f623ed4c67c2a1fe72bb2d8d2c546a1a5093
MoEzwawi <142166624+MoEzwawi@users.noreply.github.com>
2024-01-25 11:55:47 +0100

    Update BooksRunner.java

--- src/main/java/MoEzwawi/BES7L3/composite_design_pattern/BooksRunner.java
+++ src/main/java/MoEzwawi/BES7L3/composite_design_pattern/BooksRunner.java
@@ -20,6 +20,7 @@ public class BooksRunner implements CommandLineRunner {
         section2.addMultipleContent(List.of(section1,page3,page4));
         Book book1 = new Book("Tre uomini e una gamba",12.90,List.of("Aldo","Giovanni","Giacomo"));
         book1.addMultipleContent(List.of(section2,page5));
+        System.out.println("CIAO A TUTTI!");
         book1.print();
         System.out.println(book1.getPageNumber());
     }
